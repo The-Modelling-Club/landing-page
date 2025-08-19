c562350b27fd25dec917ffa48847537424421613
Evans Elabo <evans.elabo@amalitech.com>
2025-08-19 11:25:06 +0000

    feat(article_page): update article fetching to use ID and add PdfViewer component

--- src/app/(landing)/articles/[slug]/page.tsx
+++ src/app/(landing)/articles/[slug]/page.tsx
@@ -4,30 +4,29 @@ import { RichText } from "@/components/text-editor";
 import { urlFor } from "@/lib/url-for";
 import { ArticleInterface } from "@/types/articles.interface";
 import { AuthorInterface } from "@/types/authors.interface";
-import { findAllArticles, findOneArticle } from "@/utils/sanity-queries";
+import { findAllArticles, findManuScriptById } from "@/utils/sanity-queries";
 import { Metadata } from "next";
 import { PortableText } from "next-sanity";
 import Image from "next/image";
 import React from "react";
-
-type Props = {
-  params: {
-    slug: string;
-  };
-};
+import PdfViewer from "../_components/pdf-viewer";
 
 export async function generateStaticParams() {
   const articles: ArticleInterface[] = await client.fetch(findAllArticles);
-  const slugRoutes = articles.map((article: ArticleInterface) => article.slug);
+  const idRoutes = articles.map((article: ArticleInterface) => article.id);
 
-  return slugRoutes.map((slug) => ({
-    slug,
+  return idRoutes.map((id) => ({
+    id,
   }));
 }
 
-export async function generateMetadata({ params }: { readonly params: Promise<{ slug: string }> }): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  readonly params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
   const { slug } = await params;
-  const data: ArticleInterface = await client.fetch(findOneArticle, {
+  const data: ArticleInterface = await client.fetch(findManuScriptById, {
     slug,
   });
 
@@ -40,13 +39,20 @@ export async function generateMetadata({ params }: { readonly params: Promise<{ 
 
   return {
     title: `${data.title} | The Modeling Club - KNUST`,
-    description: data.description || `Read about ${data.title} on The Modeling Club's engineering insights page.`,
+    description:
+      data.description ||
+      `Read about ${data.title} on The Modeling Club's engineering insights page.`,
     keywords: `engineering, ${data.title.toLowerCase()}, computer-aided engineering, CAE, KNUST`,
-    authors: data.authors?.map((author: AuthorInterface) => ({ name: author.author_name })) || [],
+    authors:
+      data.authors?.map((author: AuthorInterface) => ({
+        name: author.author_name,
+      })) || [],
     openGraph: {
       title: data.title,
-      description: data.description || `Read about ${data.title} on The Modeling Club's engineering insights page.`,
-      url: `https://themodelingclub.com/articles/${data.slug}`,
+      description:
+        data.description ||
+        `Read about ${data.title} on The Modeling Club's engineering insights page.`,
+      url: `https://themodelingclub.com/articles/${data.id}`,
       type: "article",
       images: data.image_url
         ? [
@@ -58,27 +64,37 @@ export async function generateMetadata({ params }: { readonly params: Promise<{ 
             },
           ]
         : [],
-      authors: data.authors?.map((author: AuthorInterface) => author.author_name) || [],
+      authors:
+        data.authors?.map((author: AuthorInterface) => author.author_name) ||
+        [],
     },
     twitter: {
       card: "summary_large_image",
       title: data.title,
-      description: data.description || `Read about ${data.title} on The Modeling Club's engineering insights page.`,
+      description:
+        data.description ||
+        `Read about ${data.title} on The Modeling Club's engineering insights page.`,
       images: data.image_url ? [data.image_url] : [],
     },
     alternates: {
-      canonical: `/articles/${data.slug}`,
+      canonical: `/articles/${data.id}`,
     },
   };
 }
 
-export default async function ArticlePage({ params }: { readonly params: Promise<{ slug: string }> }) {
+export default async function ArticlePage({
+  params,
+}: {
+  readonly params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
-  const data: ArticleInterface = await client.fetch(findOneArticle, {
+  const data: ArticleInterface = await client.fetch(findManuScriptById, {
     slug,
   });
 
-  const authors = data.authors.map((author: AuthorInterface) => author.author_name);
+  const authors = data?.authors.map(
+    (author: AuthorInterface) => author.author_name
+  );
 
   return (
     <section className="mx-auto flex w-full flex-col items-center justify-center p-4 md:w-[55dvw]">
@@ -86,16 +102,26 @@ export default async function ArticlePage({ params }: { readonly params: Promise
         <div className="mt-6">
           <BackButton />
         </div>
-        <p className="py-4 text-3xl font-semibold leading-[1.6] text-neutral-700 md:text-5xl md:leading-[4rem] lg:text-center">{data.title}</p>
+        <p className="py-4 text-3xl font-semibold leading-[1.6] text-neutral-700 md:text-5xl md:leading-[4rem] lg:text-center">
+          {data.title}
+        </p>
         <div className="my-5 flex flex-row items-center gap-2 md:gap-x-4 lg:justify-center">
           <section className="flex flex-col items-start justify-center lg:flex-row lg:items-center">
             <p className="flex items-center justify-center">
-              <span className="font-normal">{authors ? `Authored by: ${authors.join(", ")}` : "Unknown Author"}</span>
+              <span className="font-normal">
+                {authors
+                  ? `Authored by: ${authors.join(", ")}`
+                  : "Unknown Author"}
+              </span>
             </p>
-            <span className="hidden px-2 text-lg text-gray-500 md:block">&#x2022;</span>
+            {authors.length > 1 && (
+              <span className="hidden px-2 text-lg text-gray-500 md:block">
+                &#x2022;
+              </span>
+            )}
           </section>
         </div>
-        <div className="my-14 w-full md:h-[20rem]">
+        <div className="my-14 w-full hidden md:h-[20rem]">
           <Image
             src={urlFor(data.image_url).url()}
             alt={data.title}
@@ -106,6 +132,10 @@ export default async function ArticlePage({ params }: { readonly params: Promise
           />
         </div>
         <PortableText value={data.content} components={RichText} />
+        <PdfViewer
+          url={data.manuscriptUrl}
+          is_downloadable={data.is_downloadable}
+        />
       </div>
     </section>
   );
